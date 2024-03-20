a7ac2dc00b2c938c03c7fbef0474a69edb0a66f0
Filippa <filippala03@gmail.com>
2024-03-20 15:33:58 +0100

    a

--- js/script.js
+++ js/script.js
@@ -27,3 +27,18 @@ inputFile.addEventListener('change', function () {
     }
 })
 
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: -20,
+      depth: 120,
+      modifier: 3,
+      slideShadows: true,
+    },
+  });
